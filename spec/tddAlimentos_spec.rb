06376af236b46185a944b6f627ba4c933d94e452
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-12 10:43:01 +0000

    Creado método que comprueba que existen las proteínas para el alimento

--- spec/tddAlimentos_spec.rb
+++ spec/tddAlimentos_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe TddAlimentos do
 
   before (:all) do
-    @cowMeat = Alimento.new("Carne de Vaca");
-    @lambMeat = Alimento.new("Carne de Cordero");
-    @chocolate = Alimento.new("Chocolate");
-    @cheese = Alimento.new("Queso");
+    @cowMeat = Alimento.new("Carne de Vaca", 21.1);
+    @lambMeat = Alimento.new("Carne de Cordero", 18.0);
+    @chocolate = Alimento.new("Chocolate", 5.3);
+    @cheese = Alimento.new("Queso", 25.0);
   end
 
   it "has a version number" do
@@ -17,6 +17,13 @@ RSpec.describe TddAlimentos do
       expect(@lambMeat.name).to eq("Carne de Cordero")
       expect(@chocolate.name).to eq("Chocolate")
       expect(@cheese.name).to eq("Queso")
+    end
+
+    it "existen las proteínas para el alimento" do
+      expect(@cowMeat.protein).to eq(21.1)
+      expect(@lambMeat.protein).to eq(18.0)
+      expect(@chocolate.protein).to eq(5.3)
+      expect(@cheese.protein).to eq(25.0)
     end
   end
 
