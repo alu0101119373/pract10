6b4e5e282a77fbbab11fbbaddaa514340755d06c
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-12 10:39:36 +0000

    Creado método que prueba que existe un nombre para la clase alimento

--- spec/tddAlimentos_spec.rb
+++ spec/tddAlimentos_spec.rb
@@ -1,14 +1,23 @@
 RSpec.describe TddAlimentos do
 
   before (:all) do
-    @cowMeat = Alimento.new;
-    @lambMeat = Alimento.new;
-    @chocolate = Alimento.new;
-    @cheese = Alimento.new;
+    @cowMeat = Alimento.new("Carne de Vaca");
+    @lambMeat = Alimento.new("Carne de Cordero");
+    @chocolate = Alimento.new("Chocolate");
+    @cheese = Alimento.new("Queso");
   end
 
   it "has a version number" do
     expect(TddAlimentos::VERSION).not_to be nil
+  end
+
+  context "probando los atributos de la clase Alimento" do
+    it "existe un nombre para el alimento" do
+      expect(@cowMeat.name).to eq("Carne de Vaca")
+      expect(@lambMeat.name).to eq("Carne de Cordero")
+      expect(@chocolate.name).to eq("Chocolate")
+      expect(@cheese.name).to eq("Queso")
+    end
   end
 
 end
