d49e8fb5dae23a5734df2f112b9ec3fc7ea00f38
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-12 11:29:15 +0000

    Implementado método 'to_s' para obtener el alimento formateado

--- lib/tddAlimentos/alimento.rb
+++ lib/tddAlimentos/alimento.rb
@@ -6,4 +6,8 @@ class Alimento
     @name, @protein, @carbohydrates, @lipids, @gei, @terrain = name, protein, carbohydrates, lipids, gei, terrain
   end
 
+  def to_s
+    "Nombre: #{@name}\nProteínas (g): #{@protein}\nCarbohidratos (g): #{@carbohydrates}\nLípidos (g): #{@lipids}\nGEI (kgCO2eq): #{@gei}\nTerreno (m2 año): #{@terrain}\n"
+  end
+
 end
