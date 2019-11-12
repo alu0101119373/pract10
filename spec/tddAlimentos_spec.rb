00c3b10f893a7f25c9097e084a183de9b935623b
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-12 10:49:05 +0000

    Creado método que comprueba si existen los carbohidratos en el alimento

--- spec/tddAlimentos_spec.rb
+++ spec/tddAlimentos_spec.rb
@@ -1,10 +1,10 @@
 RSpec.describe TddAlimentos do
 
   before (:all) do
-    @cowMeat = Alimento.new("Carne de Vaca", 21.1);
-    @lambMeat = Alimento.new("Carne de Cordero", 18.0);
-    @chocolate = Alimento.new("Chocolate", 5.3);
-    @cheese = Alimento.new("Queso", 25.0);
+    @cowMeat = Alimento.new("Carne de Vaca", 21.1, 0.0);
+    @lambMeat = Alimento.new("Carne de Cordero", 18.0, 0.0);
+    @chocolate = Alimento.new("Chocolate", 5.3, 47.0);
+    @cheese = Alimento.new("Queso", 25.0, 1.3);
   end
 
   it "has a version number" do
@@ -24,6 +24,13 @@ RSpec.describe TddAlimentos do
       expect(@lambMeat.protein).to eq(18.0)
       expect(@chocolate.protein).to eq(5.3)
       expect(@cheese.protein).to eq(25.0)
+    end
+    
+    it "existen los carbohidratos para el alimento" do
+      expect(@cowMeat.carbohydrates).to eq(0.0)
+      expect(@lambMeat.carbohydrates).to eq(0.0)
+      expect(@chocolate.carbohydrates).to eq(47.0)
+      expect(@cheese.carbohydrates).to eq(1.3)
     end
   end
 
