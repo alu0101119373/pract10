6d7b77006342a2ae800353c9feaf112aba00f37d
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-25 22:21:30 +0000

    Creada clase Plato

--- lib/tddAlimentos.rb
+++ lib/tddAlimentos.rb
@@ -1,6 +1,7 @@
 require "tddAlimentos/version"
 require "tddAlimentos/alimento"
 require "tddAlimentos/lista.rb"
+require "tddAlimentos/plato.rb"
 
 module TddAlimentos
   class Error < StandardError; end
