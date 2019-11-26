7b9445d4059a0fe513b61255a35458844a4f5ecc
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-26 17:17:05 +0000

    Implementado atributo para almacenar las cantidades de los alimentos en gramos

--- lib/tddAlimentos/plato.rb
+++ lib/tddAlimentos/plato.rb
@@ -1,10 +1,13 @@
 class Plato
-    attr_reader :name, :food
+    attr_reader :name, :food, :quantities
 
-    def initialize (name, alimentos = [])
+    def initialize (name, alimentos = [], gramos = [])
         @name = name
 
         @food = List.new
         @food.insert_more(alimentos)
+
+        @quantities = List.new
+        @quantities.insert_more(gramos)
     end
 end
