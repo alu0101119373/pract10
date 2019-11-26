75fb5cb53de5e7b251d48a7ccb39b169228b184b
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-26 17:38:25 +0000

    Implementado atributo y cálculo para el porcentaje de lípidos

--- lib/tddAlimentos/plato.rb
+++ lib/tddAlimentos/plato.rb
@@ -1,5 +1,5 @@
 class Plato
-    attr_reader :name, :food, :quantities, :vct, :percentage_proteins
+    attr_reader :name, :food, :quantities, :vct, :percentage_proteins, :percentage_lipids
 
     def initialize (name, alimentos = [], gramos = [])
         @name = name
@@ -14,6 +14,7 @@ class Plato
 
         # Calculamos los porcentajes
         @percentage_proteins = calc_perc_proteins
+        @percentage_lipids = calc_perc_lipids
     end
 
     def calc_vct
@@ -34,6 +35,17 @@ class Plato
 
         for i in 0...alimentos.size
             result += (gramos[i] * alimentos[i]) * 4
+        end
+        result /= @vct
+    end
+
+    def calc_perc_lipids
+        alimentos = @food.collect { |x| x.lipids }
+        gramos = @quantities.collect { |x| x/100.0 }
+        result = 0.0
+
+        for i in 0...alimentos.size
+            result += (gramos[i] * alimentos[i]) * 9
         end
         result /= @vct
     end
