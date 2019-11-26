6f450a4dff8d38f080afc5f1f8f98f50447cefa3
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-26 18:03:13 +0000

    Creada clase PlatoAmbiental

--- lib/tddAlimentos.rb
+++ lib/tddAlimentos.rb
@@ -2,6 +2,7 @@ require "tddAlimentos/version"
 require "tddAlimentos/alimento"
 require "tddAlimentos/lista.rb"
 require "tddAlimentos/plato.rb"
+require "tddAlimentos/platoAmbiental.rb"
 
 module TddAlimentos
   class Error < StandardError; end
---
+++ lib/tddAlimentos/platoAmbiental.rb
@@ -0,0 +1,2 @@
+class PlatoAmbiental
+end
