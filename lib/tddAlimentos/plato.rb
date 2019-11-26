7382d9168f92cad8a655e8d9e5c4c7da94bce15f
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-26 17:36:44 +0000

    Implementado atributo y cálculo para el porcentaje de proteínas

--- lib/tddAlimentos/plato.rb
+++ lib/tddAlimentos/plato.rb
@@ -1,5 +1,5 @@
 class Plato
-    attr_reader :name, :food, :quantities, :vct
+    attr_reader :name, :food, :quantities, :vct, :percentage_proteins
 
     def initialize (name, alimentos = [], gramos = [])
         @name = name
@@ -11,6 +11,9 @@ class Plato
         @quantities.insert_more(gramos)
 
         @vct = calc_vct
+
+        # Calculamos los porcentajes
+        @percentage_proteins = calc_perc_proteins
     end
 
     def calc_vct
@@ -22,5 +25,16 @@ class Plato
             result += (gramos[i] * alimentos[i])
         end
         result
+    end
+
+    def calc_perc_proteins
+        alimentos = @food.collect { |x| x.protein }
+        gramos = @quantities.collect { |x| x/100.0 }
+        result = 0.0
+
+        for i in 0...alimentos.size
+            result += (gramos[i] * alimentos[i]) * 4
+        end
+        result /= @vct
     end
 end
