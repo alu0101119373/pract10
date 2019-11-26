9fa5dff94ad598ea8a7448d07843260c637f7958
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-26 17:32:21 +0000

    Implementado cálculo del VCT

--- lib/tddAlimentos/plato.rb
+++ lib/tddAlimentos/plato.rb
@@ -1,5 +1,5 @@
 class Plato
-    attr_reader :name, :food, :quantities
+    attr_reader :name, :food, :quantities, :vct
 
     def initialize (name, alimentos = [], gramos = [])
         @name = name
@@ -9,5 +9,18 @@ class Plato
 
         @quantities = List.new
         @quantities.insert_more(gramos)
+
+        @vct = calc_vct
+    end
+
+    def calc_vct
+        alimentos = @food.collect { |x| x.energetic_value }
+        gramos = @quantities.collect { |x| x/100.0 }
+        result = 0.0
+
+        for i in 0...alimentos.size
+            result += (gramos[i] * alimentos[i])
+        end
+        result
     end
 end
