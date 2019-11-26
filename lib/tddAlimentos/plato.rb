6d267df2a1643346313472f8fe1112f3999d8399
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-26 17:57:22 +0000

    Implementado método 'to_s' para imprimir los datos del plato (formateo)

--- lib/tddAlimentos/plato.rb
+++ lib/tddAlimentos/plato.rb
@@ -18,6 +18,17 @@ class Plato
         @percentage_carbohydrates = calc_perc_carbohydrates
     end
 
+    def to_s
+        alimentos = @food.collect { |x| x.name }
+        gramos = @quantities.collect { |x| x }
+        result = ""
+
+        result += "Nombre del plato: #{@name}.\nIngredientes:\n"
+        for i in 0...alimentos.size
+            result += "- #{alimentos[i]} (#{gramos[i]} gr)\n"
+        end
+        result += "Kilocalorias totales: #{self.vct} kcal"
+    end
     def calc_vct
         alimentos = @food.collect { |x| x.energetic_value }
         gramos = @quantities.collect { |x| x/100.0 }
