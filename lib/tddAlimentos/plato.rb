7f553e92dffe79c6cfc0cd7b7163ec572e443cae
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-26 17:09:20 +0000

    Atributo que almacena un conjunto de alimentos

--- lib/tddAlimentos/plato.rb
+++ lib/tddAlimentos/plato.rb
@@ -1,7 +1,10 @@
 class Plato
-    attr_reader :name
+    attr_reader :name, :food
 
-    def initialize (name)
+    def initialize (name, alimentos = [])
         @name = name
+
+        @food = List.new
+        @food.insert_more(alimentos)
     end
 end
