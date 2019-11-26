e9705df374d301035a917bb6b7ad31d0714f3681
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-26 18:26:20 +0000

    Implementado atributo gei en la clase PlatoAmbiental

--- lib/tddAlimentos/platoAmbiental.rb
+++ lib/tddAlimentos/platoAmbiental.rb
@@ -1,7 +1,22 @@
 class PlatoAmbiental < Plato
 
+    attr_reader :gei
+
     def initialize (name, alimentos = [], gramos = [])
         super(name, alimentos, gramos)
+
+        @gei = calc_gei
     end
-    
+
+    def calc_gei
+        datos = @food.collect { |x| x.gei }
+        gramos = @quantities.collect { |x| x/100.0 }
+        result = 0.0
+
+        for i in 0...datos.size
+            result += datos[i] * gramos[i]
+        end
+        result
+    end
+
 end
