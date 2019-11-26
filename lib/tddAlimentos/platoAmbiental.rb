a69870c41b85cf4f56e6091a67361032217f1def
alu0101119373 <alu0101119373@ull.edu.es>
2019-11-26 18:28:19 +0000

    Implementado atributo terrain en la clase PlatoAmbiental

--- lib/tddAlimentos/platoAmbiental.rb
+++ lib/tddAlimentos/platoAmbiental.rb
@@ -1,15 +1,27 @@
 class PlatoAmbiental < Plato
 
-    attr_reader :gei
+    attr_reader :gei, :terrain
 
     def initialize (name, alimentos = [], gramos = [])
         super(name, alimentos, gramos)
 
         @gei = calc_gei
+        @terrain = calc_terrain
     end
 
     def calc_gei
         datos = @food.collect { |x| x.gei }
+        gramos = @quantities.collect { |x| x/100.0 }
+        result = 0.0
+
+        for i in 0...datos.size
+            result += datos[i] * gramos[i]
+        end
+        result
+    end
+
+    def calc_terrain
+        datos = @food.collect { |x| x.terrain }
         gramos = @quantities.collect { |x| x/100.0 }
         result = 0.0
 
