a63ffca5a4cbc30e68c46f3f285d6c8543a854b3
alu0101119373 <alu0101119373@ull.edu.es>
2019-12-10 15:23:24 +0000

    Implementado método que calcula la huella nutricional de un plato

--- lib/tddAlimentos/platoAmbiental.rb
+++ lib/tddAlimentos/platoAmbiental.rb
@@ -17,6 +17,28 @@ class PlatoAmbiental < Plato
         result += "\nTerreno: #{@terrain.round(2)} m2 / año"
     end
 
+    def nutritional_footprint
+        kcal_impact = 0
+        if @vct < 670
+            kcal_impact = 1
+        elsif @vct > 830
+            kcal_impact = 3
+        else
+            kcal_impact = 2
+        end
+
+        gei_impact = 0
+        if @gei < 800
+            gei_impact = 1
+        elsif @gei > 1200
+            gei_impact = 3
+        else
+            gei_impact = 2
+        end
+
+        (kcal_impact + gei_impact) / 2.0
+    end
+
     def calc_gei
         datos = @food.collect { |x| x.gei }
         gramos = @quantities.collect { |x| x/100.0 }
